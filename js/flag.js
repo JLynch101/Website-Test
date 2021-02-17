e585d0df999ec7fb2a6bae93de7dd485531ff320
Joe Lynch <48054458+JLynch101@users.noreply.github.com>
2021-02-17 09:50:32 +0000

    Add files via upload

---
+++ js/flag.js
@@ -0,0 +1,9 @@
+function z() {
+    var a = document.getElementById("test");
+    if ((a.value == "YAGDADD1873271JIJ") || (a.value == "YAGDADD1873271JIJ")) {
+        document.getElementById('err').innerHTML = '  Correct flag, Well Done!';
+    } else {
+        document.getElementById('err').innerHTML = '	Incorrect flag! ';
+
+    }
+}
---
+++ js/flag2.js
@@ -0,0 +1,9 @@
+function z() {
+    var a = document.getElementById("test");
+    if ((a.value == "UUADFAD77900DAD") || (a.value == "UUADFAD77900DAD")) {
+        document.getElementById('err').innerHTML = 'Correct flag, Well Done!';
+    } else {
+        document.getElementById('err').innerHTML = 'Incorrect flag! ';
+
+    }
+}// JavaScript Document
